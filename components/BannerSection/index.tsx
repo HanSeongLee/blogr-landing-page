c68f9708a060c788808c20f379f042f51533df19
HanSeong Lee <jkhanseong@gmail.com>
2022-05-05 13:25:13 +0900

    ADD | BannerSection on mobile

---
+++ components/BannerSection/index.tsx
@@ -0,0 +1,34 @@
+import React, {HTMLAttributes} from 'react';
+import styles from './style.module.scss';
+import Container from '../Container';
+import cn from 'classnames';
+
+const BannerSection: React.FC<HTMLAttributes<HTMLDivElement>> = ({ className, ...props }) => {
+    return (
+        <div className={cn(styles.bannerSection, className)}
+             {...props}
+        >
+            <div className={styles.imageWrapper}>
+                <img className={styles.image}
+                     src={'/img/illustration-phones.svg'}
+                     alt={''}
+                />
+            </div>
+
+            <Container className={styles.container}>
+                <div className={styles.content}>
+                    <h2 className={styles.title}>
+                        State of the Art Infrastructure
+                    </h2>
+                    <p className={styles.description}>
+                        With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast
+                        connectivity. This ensures your site will load instantly, no matter where your readers are,
+                        keeping your site competitive.
+                    </p>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default BannerSection;
